b622f80ebd8d667415d3672f4e585d5cbea480e1
LapisLadzuli <marius.thill04@yahoo.com>
2025-02-17 02:36:53 +0100

    PlayerMovement and BuggyMovement Redone
    Das was ich verkackt havbe ist wieder drinne

--- Assets/Scripts/player/pl_cam_move.cs
+++ Assets/Scripts/player/pl_cam_move.cs
@@ -1,11 +1,20 @@
 using UnityEngine;
 
+interface IInteractable
+{
+    public void Interact();
+}
 public class pl_cam_move : MonoBehaviour
 {
     GameObject cam;
     [SerializeField]float sens;
     float currentY =0;
+
     
+    [SerializeField]
+    [Range(1, 10)]
+    float interactorRange;
+
     void Start()
     {
         cam = GameObject.FindGameObjectWithTag("MainCamera");
@@ -21,5 +30,22 @@ public class pl_cam_move : MonoBehaviour
         currentY = Mathf.Clamp(currentY, -60, 60);
         cam.transform.localEulerAngles = Vector3.right * currentY;
         transform.localEulerAngles += Vector3.up * inputX ;
+
+        Debug.DrawRay(cam.transform.position, cam.transform.forward * interactorRange);
+
+        Ray r = new Ray(cam.transform.position, cam.transform.forward);
+        if (Physics.Raycast(r, out RaycastHit hitInfo, interactorRange))
+        {
+
+            if (hitInfo.collider.gameObject.TryGetComponent(out IInteractable interactObject))
+            {
+                Debug.Log(interactObject);
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    interactObject.Interact();
+                }
+            }
+        }
+
     }
 }
--- Assets/Scripts/player/pl_interactor.cs
+++ Assets/Scripts/player/pl_interactor.cs
@@ -1,20 +1,22 @@
 using UnityEngine;
-interface IInteractable
-{
-    public void Interact();
-}
+
 public class pl_interactor : MonoBehaviour
 {
-    [SerializeField] Transform interactorSource;
+    GameObject interactorSource;
     [SerializeField]
     [Range(1,10)]
     float interactorRange;
+
+    private void Awake()
+    {
+        interactorSource = GameObject.FindGameObjectWithTag("MainCamera");
+    }
     void Update()
     {
-        Debug.DrawRay(interactorSource.position, interactorSource.forward * interactorRange);
-        
-        Ray r = new Ray(interactorSource.position, interactorSource.forward);
-            if (Physics.Raycast(r, out RaycastHit hitInfo, interactorRange))
+        Debug.DrawRay(interactorSource.transform.position, interactorSource.transform.forward * interactorRange);
+
+        Ray r = new Ray(interactorSource.transform.position, interactorSource.transform.forward);
+        if (Physics.Raycast(r, out RaycastHit hitInfo, interactorRange))
             {
             
                 if(hitInfo.collider.gameObject.TryGetComponent(out IInteractable interactObject))
